26fc015e2abe6d55d27203eafab6158cba4bf23c
HelbertAlexeiv <helbertalexeivcorreauribe@gmail.com>
2026-08-05 13:25:41 -0500

    feat: create menu endpoint

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from . import views
+from rest_framework.routers import DefaultRouter
+from .views import MenuItemsView, SingleMenuItemView
+
+#urls examples with path
 
 urlpatterns =[
     #path('', views.sayHello, name="hello")
     path('', views.index, name='index'),
+    path('menu/<int:pk>', SingleMenuItemView.as_view(), name='menu'),
+    path('menu/items', MenuItemsView.as_view(), name='menu_items'),
 ]
+
+
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import Booking, Menu
+from .serializers import BookingSerializer, MenuSerializer
 # Create your views here.
 
+#Examples views
 def sayHello(request):
     return HttpResponse("Hello World")
 
 def index(request):
     return render(request, 'index.html', {})
+
+#-------app views------
+class MenuItemsView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
